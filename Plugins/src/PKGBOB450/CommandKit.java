4c6b0ffc7b8b09532fa89fa12a6053ac2620e69d
BOB450 <bwvoskuil@gmail.com>
2021-10-28 12:27:05 -0400

    added new plugin
    now as a basic help command set up

---
+++ Plugins/src/PKGBOB450/CommandKit.java
@@ -0,0 +1,18 @@
+package PKGBOB450;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandKit implements CommandExecutor {
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            // Here we need to give items to our player
+            player.sendMessage("you ran the help command helpsmp");
+        }
+        return true;
+    }
+}
---
+++ Plugins/src/PKGBOB450/Main.java
@@ -0,0 +1,17 @@
+package PKGBOB450;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("THIS PLUGin HAS BEEN ENABLED");
+        this.getCommand("helpsmp").setExecutor(new CommandKit());
+        super.onEnable();
+
+    }
+
+}
